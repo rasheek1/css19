f186363c5eafcda480b3c3116736bce1271173de
rasheek1 <rashiquiminott@mec.science>
2019-07-29 15:16:52 -0400

    day5

---
+++ day5/alarm.html/alarm.js
@@ -0,0 +1,12 @@
+function myAlarm(m)
+{
+    console.log("WakeUp");
+    console.log("Hey, kobe, it's " + m);
+    console.log ("WAKE UP, WAKE UP, WAKE UP!!")
+}
+
+
+myAlarm("6:30am");
+myAlarm("6:45am");
+myAlarm("6:55am");
+
